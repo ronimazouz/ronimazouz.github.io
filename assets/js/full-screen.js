e299af1a6385662a8fb362252363ce7d58cd3f6d
Roni Mazouz <rrmazouz@aol.com>
2019-02-09 09:28:02 -0500

    tuto test

---
+++ assets/js/full-screen.js
@@ -0,0 +1,51 @@
+            var isFullscreen = false;
+            
+            function fullscreen(){      
+                var d = {};
+                var speed = 300;
+                if(!isFullscreen){ // MAXIMIZATION
+                    d.width = "100%";
+                    d.height = "100%"; 
+                    isFullscreen = true;
+                    $("#div-margin").slideUp(speed);
+                    $("#div-messenger").slideUp(speed)
+                
+                }
+                else{ // MINIMIZATION            
+                    d.width = "100%";
+                    d.height = "50vh";            
+                    isFullscreen = false;
+                    $("#div-margin").slideDown(speed);
+                    $("#div-messenger").slideDown(speed+200)
+                    
+                }
+                $("#div-video-big").animate(d,speed);         
+            }
+
+
+$("button").click(function() {
+  $(this).toggleClass('click');
+});
+
+$("button").click(function() {
+  $(this).toggleClass('rec');
+});
+
+$("button").click(function(){
+ $(this).toggleClass('rotator')  ; 
+})
+
+function changeimg()
+{
+if(document.getElementById("expand").src = 'maximize.svg' )
+document.getElementById("expand").src = 'minimize.svg';
+else
+document.getElementById("expand").src = 'maximize.svg';
+}
+
+    
+
+
+
+
+
